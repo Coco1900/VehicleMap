4d686bbe603d0b03feee55484b5979eb8949f1af
Coco1900 <46676720+Coco1900@users.noreply.github.com>
2023-02-28 13:13:38 +0100

    init + base soap

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,44 @@
+import { splitNsName } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { map } from 'rxjs';
+import {SoapMethod} from 'soap'
+import { SoapService } from '../service/soap/soap.service';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent{
+
+
+  speed!: number;
+  distance!: number;
+  nbstops!: number;
+  timestop!: number;
+
+  resultat!: number;
+
+  constructor(private soapService: SoapService) { }
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
+    console.log("COUCOU");
+
+    const speed = form.value.speed;
+    const distance = form.value.distance;
+    const nbstops = form.value.nbStops;
+    const timestops = form.value.timeStop;
+
+    console.log(speed, distance, nbstops, timestops);
+    this.soapService.calculDuration(speed, distance, nbstops, timestops).pipe(
+      map(value => this.resultat = value)
+    ).subscribe();
+  }
+
+  
+
+  
+
+}
---
+++ src/app/service/soap/soap.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SoapService {
+
+  private uri = 'http://localhost:8000';
+  private options = { responseType: 'text' as 'json' };
+
+  constructor(private http: HttpClient) { }
+
+  calculDuration(speed: number, distance: number, nbstops: number, timeStop: number): Observable<any> {
+    const body = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:spy="spyne.examples.hello.soap">\
+      <soapenv:Header/>
+      <soapenv:Body>
+        <spy:duration_time>
+          <spy:speed>${speed}</spy:speed>
+          <spy:distance>${distance}</spy:distance>
+          <spy:nbstops>${nbstops}</spy:nbstops>
+          <spy:timeStop>${timeStop}</spy:timeStop>
+        </spy:duration_time>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
+    return this.http.post<any>(this.uri, body, this.options).pipe(
+      map(value => {
+        const data = value.split("duration_timeResult");
+        let res = data[1];
+        res = res.replace(">","");
+        res = res.replace("</tns:","");
+        return res;
+      })
+    );
+  }
+
+}
